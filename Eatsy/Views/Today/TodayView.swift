5c5d2981deaa7715040811a70868966f7633deaf
Henry <henry.yohanes7@gmail.com>
2025-09-09 12:00:51 +0700

    update today view

--- Eatsy/Views/Today/TodayView.swift
+++ Eatsy/Views/Today/TodayView.swift
@@ -9,6 +9,29 @@ import SwiftUI
 
 struct TodayView: View {
     var body: some View {
-        Text("TodayView")
+        VStack{
+            HStack{
+                Text("Today's Meal Plan").bold()
+                Spacer()
+                Button(action:{}){
+                    Image(systemName: "plus")
+                }
+            }.padding(12)
+            
+            Button(action:{}){
+                Text("Get Meal Plan")
+            }.foregroundStyle(.white)
+                .padding(.horizontal, 100)
+                .padding(.vertical, 10)
+                .background(Color.green)
+            
+            HStack {
+                Text("")
+            }
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
     }
+}
+
+#Preview {
+    ContentView()
 }
