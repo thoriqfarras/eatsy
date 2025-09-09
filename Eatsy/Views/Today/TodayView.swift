e9d8a0d6469af34b4dcaf0b2a35c8cb0c8a41916
Henry <henry.yohanes7@gmail.com>
2025-09-09 11:37:24 +0700

    add today view

---
+++ Eatsy/Views/Today/TodayView.swift
@@ -0,0 +1,14 @@
+//
+//  TodayView.swift
+//  Eatsy
+//
+//  Created by Mac on 09/09/25.
+//
+
+import SwiftUI
+
+struct TodayView: View {
+    var body: some View {
+        Text("TodayView")
+    }
+}
