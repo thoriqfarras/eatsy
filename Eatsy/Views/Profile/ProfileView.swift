480bc136407ec4405ed36a77ce348c3c828a5a93
MaulidyaAyu <neenaardiena@gmail.com>
2025-09-10 22:08:04 +0700

    nambah shadow+warna global

--- Eatsy/Views/Profile/ProfileView.swift
+++ Eatsy/Views/Profile/ProfileView.swift
@@ -22,13 +22,13 @@ struct ProfileView: View {
                 .bold()
                 .foregroundColor(.white)
                 .padding(12)
-                .background(.green)
+                .background(Color("PrimaryGreen"))
                 .clipShape(Capsule())
             }
             .padding()
             .background(.white)
             .clipShape(RoundedRectangle(cornerRadius: 8))
-            .shadow(color: Color.black.opacity(0.08), radius: 6)
+            .eatsyShadow()
             .padding([.horizontal, .top])
             
             VStack (alignment: .leading) {
@@ -45,13 +45,13 @@ struct ProfileView: View {
                 .padding()
                 .background(.white)
                 .clipShape(RoundedRectangle(cornerRadius: 8))
-                .shadow(color: Color.black.opacity(0.08), radius: 6)
+                .eatsyShadow()
                 .padding(.horizontal)
             }
             Spacer()
         }
-//        .navigationTitle("Profile")
-//        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle("Profile")
+        .navigationBarTitleDisplayMode(.inline)
     }}
 #Preview {
     ProfileView()
@@ -67,6 +67,7 @@ struct ProfileItem: View {
             Spacer()
             Text(val)
                 .font(.caption)
+                .bold(true)
                 .foregroundStyle(Color (.systemGray2))
             Image(systemName: "chevron.right")
                 .foregroundColor(Color (.systemGray2))
